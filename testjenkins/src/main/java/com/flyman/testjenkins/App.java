a15ca5a46fc27363b825b5f035dfce1ed5c5475e
flyman <fanyy@fuiou.com>
2018-06-29 18:24:46 +0800

    项目初始化

---
+++ testjenkins/src/main/java/com/flyman/testjenkins/App.java
@@ -0,0 +1,14 @@
+package com.flyman.testjenkins;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	System.out.println("hello");
+        System.out.println( "Hello World!" );
+    }
+}
